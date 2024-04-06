4bf967df538d00652fb8fe0e18d5d34afd9a944f
spr593 <salome.perez8@gmail.com>
2024-04-06 17:32:25 -0500

    Implemented Feature2

---
+++ feature2.py
@@ -0,0 +1,13 @@
+import unittest
+
+class featureB:
+    def __init__(self, x = 0, y= 0):
+        self.x = x
+        self.y = y
+
+    def action(self):
+        return self.x/self.y
+
+class testFeatureB(unittest.TestCase):
+    def test_featureB(self):
+        self.assertIsInstance(featureB.action(), (int,float), "Not numeric result")
