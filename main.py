62255cb7cc812d375c52f67833efe3fa2f4a8bc7
spr593 <salome.perez8@gmail.com>
2024-04-06 22:44:10 -0500

    Updated tests in main

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import unittest
+from feature2 import *
+from feature3 import *
 
 if __name__=="__main__":
-    #obj = featureA(7,9)
-    suite = unittest.TestLoader().loadTestsFromTestCase(testFeature)
+    objB = featureB(7,9)
+    suite = unittest.TestLoader().loadTestsFromTestCase(testFeatureB)
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
     if result.wasSuccessful():
-        print("Tests passed")
+        print("Tests-B passed")
     else:
-        print("Tests failed")
+        print("Tests-B failed")
+    
+    objC = featureC(2,3)
+    suite = unittest.TestLoader().loadTestsFromTestCase(testFeatureC)
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
+    if result.wasSuccessful():
+        print("Tests-C passed")
+    else:
+        print("Tests-C failed")
 
