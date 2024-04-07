b784e55ffe21bd3bbf7cf3b9fec5b0518ffc74ca
spr593 <salome.perez8@gmail.com>
2024-04-06 19:03:32 -0500

    Added functions for feature3

---
+++ feature3.py
@@ -0,0 +1,13 @@
+import unittest
+
+class featureC:
+    def __init__(self, x = 0, y= 0):
+        self.x = x
+        self.y = y
+
+    def action(self):
+        return self.x+self.y
+
+class testFeatureC(unittest.TestCase):
+    def test_featureB(self):
+        self.assertIsInstance(featureC.action(), (int,float), "Not numeric result")
