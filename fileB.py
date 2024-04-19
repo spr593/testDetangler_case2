ec67a27164b3f6f7374bcac0d419830ca1253a95
spr593 <salome.perez8@gmail.com>
2024-04-19 14:24:11 -0500

    fileA and B preparation

--- fileB.py
+++ fileB.py
@@ -1,3 +1,11 @@
+def map_lists(list1: List[int], list2: List[int]) -> List[float]:
+    """
+        Maps two lists to its convolution.
+        Args:
+        Returns:
+    """
+
+
 if __name__=="__main__":
 
     print("This is File 2 from the MERGED branch")
