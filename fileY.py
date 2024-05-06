b005e131878781ba162e686208446354108b3ff9
spr593 <salome.perez8@gmail.com>
2024-05-06 12:28:20 -0500

    cleaning files

--- fileY.py
+++
@@ -1,5 +0,0 @@
-from fileX import map_lists, test_conv
-
-if __name__=="__main__":
-    print("This is File Y")
-    test_conv(map_lists([10,0],[0,-10]))
--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-import unittest
-from feature2 import *
-from feature3 import *
+from featureA import map_lists, test_conv
 
 if __name__=="__main__":
-    objB = featureB(7,9)
-    suite = unittest.TestLoader().loadTestsFromTestCase(testFeatureB)
-    runner = unittest.TextTestRunner()
-    result = runner.run(suite)
-    if result.wasSuccessful():
-        print("Tests-B passed")
-    else:
-        print("Tests-B failed")
-    
-    objC = featureC(2,3)
-    suite = unittest.TestLoader().loadTestsFromTestCase(testFeatureC)
-    runner = unittest.TextTestRunner()
-    result = runner.run(suite)
-    if result.wasSuccessful():
-        print("Tests-C passed")
-    else:
-        print("Tests-C failed")
-
+    test_conv(map_lists([10,0],[0,-10]))
