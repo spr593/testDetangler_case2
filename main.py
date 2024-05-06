1f813b86d9f84f2d589364c9b613b1ecbee7e777
spr593 <salome.perez8@gmail.com>
2024-05-06 12:46:59 -0500

    Case examples for featureA

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from featureA import map_lists, test_conv
 
 if __name__=="__main__":
-    test_conv(map_lists([10,0],[0,-10]))
+    test_conv(map_lists([3,0],[0,-3]))
+    test_conv(map_lists([0,10],[10,0]))
