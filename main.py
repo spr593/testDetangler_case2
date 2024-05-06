c463e2f6277ab97d2a96a34ffaaa72e55371dcef
spr593 <salome.perez8@gmail.com>
2024-05-06 12:48:45 -0500

    updated docs main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from featureA import map_lists, test_conv
 
 if __name__=="__main__":
+    #First test case
     test_conv(map_lists([3,0],[0,-3]))
+    #Second test case
     test_conv(map_lists([0,10],[10,0]))
