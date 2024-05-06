6b367352940bd9fe39f511ccae0805e99556cac1
spr593 <salome.perez8@gmail.com>
2024-05-06 13:02:14 -0500

    testA changes main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from featureA import map_lists, test_conv
+from featureA import map_lists, test_fA
 
 if __name__=="__main__":
     #First test case
-    test_conv(map_lists([3,0],[0,-3]))
+    test_fA(map_lists([3,0],[0,-3]))
     #Second test case
     test_conv(map_lists([0,10],[10,0]))
